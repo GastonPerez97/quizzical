46927fd11e1c34171a58393f07f2ace784286f0f
Gaston Perez <gastonperez-97@hotmail.com>
2022-01-04 16:21:02 -0300

    added first view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import "./App.css";
+import shapeTop from "./assets/images/shape-1.png";
+import shapeBottom from "./assets/images/shape-2.png";
 
 function App() {
-  return (
-    <div>
-		
-    </div>
-  );
+	return (
+		<main>
+			<img className="shape-top" src={shapeTop} alt="Shape Top" />
+			<img className="shape-bottom" src={shapeBottom} alt="Shape Bottom" />
+
+			<section className="game-intro">
+				<h1 className="game-title">Quizzical</h1>
+				<p>Answer the questions and test your knowledge!</p>
+				<button className="btn-primary">Start Quiz</button>
+			</section>
+		</main>
+	);
 }
 
 export default App;
