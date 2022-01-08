01ebeeb66ffc79fcd243c2e66a9db942b4f57e82
Gaston Perez <gastonperez-97@hotmail.com>
2022-01-08 16:04:21 -0300

    added game options styles and responsive design

--- src/App.js
+++ src/App.js
@@ -31,7 +31,6 @@ const App = () => {
 	return (
 		<main>
 			<img className="shape-top" src={shapeTop} alt="Shape Top" />
-			<img className="shape-bottom" src={shapeBottom} alt="Shape Bottom" />
 			
 			{
 				gameStarted
@@ -42,75 +41,86 @@ const App = () => {
 				:
 					<section className="game-intro">
 						<h1 className="game-title">Quizzical</h1>
-						<p>Answer the questions and test your knowledge!</p>
+						<p className="game-text">Answer the questions and test your knowledge!</p>
 
-						<div>
-							<label htmlFor="category">Select Category:</label>
+						<div className="gameOptions-container">
+							<div className="select-container">
+								<label className="custom-label" htmlFor="category">Category:</label>
 
-							<select
-								name="category"
-								id="category"
-								value={gameOptions.category}
-								onChange={handleChange}
-							>
-								<option value="">Any Category</option>
-								<option value="9">General Knowledge</option>
-								<option value="10">Entertainment: Books</option>
-								<option value="11">Entertainment: Film</option>
-								<option value="12">Entertainment: Music</option>
-								<option value="13">Entertainment: Musicals &amp; Theatres</option>
-								<option value="14">Entertainment: Television</option>
-								<option value="15">Entertainment: Video Games</option>
-								<option value="16">Entertainment: Board Games</option>
-								<option value="17">Science &amp; Nature</option>
-								<option value="18">Science: Computers</option>
-								<option value="19">Science: Mathematics</option>
-								<option value="20">Mythology</option>
-								<option value="21">Sports</option>
-								<option value="22">Geography</option>
-								<option value="23">History</option>
-								<option value="24">Politics</option>
-								<option value="25">Art</option>
-								<option value="26">Celebrities</option>
-								<option value="27">Animals</option>
-								<option value="28">Vehicles</option>
-								<option value="29">Entertainment: Comics</option>
-								<option value="30">Science: Gadgets</option>
-								<option value="31">Entertainment: Japanese Anime &amp; Manga</option>
-								<option value="32">Entertainment: Cartoon &amp; Animations</option>
-							</select>
+								<select
+									name="category"
+									id="category"
+									className="custom-select"
+									value={gameOptions.category}
+									onChange={handleChange}
+								>
+									<option value="">Any Category</option>
+									<option value="9">General Knowledge</option>
+									<option value="10">Entertainment: Books</option>
+									<option value="11">Entertainment: Film</option>
+									<option value="12">Entertainment: Music</option>
+									<option value="13">Entertainment: Musicals &amp; Theatres</option>
+									<option value="14">Entertainment: Television</option>
+									<option value="15">Entertainment: Video Games</option>
+									<option value="16">Entertainment: Board Games</option>
+									<option value="17">Science &amp; Nature</option>
+									<option value="18">Science: Computers</option>
+									<option value="19">Science: Mathematics</option>
+									<option value="20">Mythology</option>
+									<option value="21">Sports</option>
+									<option value="22">Geography</option>
+									<option value="23">History</option>
+									<option value="24">Politics</option>
+									<option value="25">Art</option>
+									<option value="26">Celebrities</option>
+									<option value="27">Animals</option>
+									<option value="28">Vehicles</option>
+									<option value="29">Entertainment: Comics</option>
+									<option value="30">Science: Gadgets</option>
+									<option value="31">Entertainment: Japanese Anime &amp; Manga</option>
+									<option value="32">Entertainment: Cartoon &amp; Animations</option>
+								</select>
+							</div>
+							
+							<div className="select-container">
+								<label className="custom-label" htmlFor="difficulty">Difficulty:</label>
 
-							<label htmlFor="difficulty">Select Difficulty:</label>
+								<select
+									name="difficulty"
+									id="difficulty"
+									className="custom-select"
+									value={gameOptions.difficulty}
+									onChange={handleChange}
+								>
+									<option value="">Any Difficulty</option>
+									<option value="easy">Easy</option>
+									<option value="medium">Medium</option>
+									<option value="hard">Hard</option>
+								</select>
+							</div>
+							
+							<div className="select-container">
+								<label className="custom-label" htmlFor="type">Type of questions:</label>
 
-							<select
-								name="difficulty"
-								id="difficulty"
-								value={gameOptions.difficulty}
-								onChange={handleChange}
-							>
-								<option value="">Any Difficulty</option>
-								<option value="easy">Easy</option>
-								<option value="medium">Medium</option>
-								<option value="hard">Hard</option>
-							</select>
-
-							<label htmlFor="type">Select Type:</label>
-
-							<select
-								name="type"
-								id="type"
-								value={gameOptions.type}
-								onChange={handleChange}
-							>
-								<option value="">Any Type</option>
-								<option value="multiple">Multiple Choice</option>
-								<option value="boolean">True / False</option>
-							</select>
+								<select
+									name="type"
+									id="type"
+									className="custom-select"
+									value={gameOptions.type}
+									onChange={handleChange}
+								>
+									<option value="">Any Type</option>
+									<option value="multiple">Multiple Choice</option>
+									<option value="boolean">True / False</option>
+								</select>
+							</div>
 						</div>
 
 						<button className="btn-primary" onClick={handleGameStart}>Start Quiz</button>
 					</section>
 			}
+
+			<img className="shape-bottom" src={shapeBottom} alt="Shape Bottom" />
 		</main>
 	);
 }
